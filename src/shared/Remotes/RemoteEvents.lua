bcc5d97b9657867c008307473c0bba288ab40ab4
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-03 22:03:40 +0200

    feat [RemoteEvents.lua]: added RemoteEvents via script

---
+++ src/shared/Remotes/RemoteEvents.lua
@@ -0,0 +1,20 @@
+--// Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--// Module Table
+local RemoteEvent = {}
+
+--// Folders 
+local RemotesFolder = ReplicatedStorage.Shared.Remotes
+local LevelRemotesFolder = Instance.new("Folder", RemotesFolder)
+LevelRemotesFolder.Name  = "LevelRemotesFolder"
+
+--// Global Remotes/Functions
+local GetAllData = Instance.new("RemoteFunction", RemotesFolder)
+GetAllData.Name = "GetAllData"
+
+--// Remotes [LevelSystem]
+local XPChanged = Instance.new("RemoteEvent", LevelRemotesFolder)
+XPChanged.Name = "XpChange"
+
+return RemoteEvent
