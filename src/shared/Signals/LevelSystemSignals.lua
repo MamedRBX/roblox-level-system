46c14e90fe9e63020b7e832b444c625b5968bc09
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-03 19:20:48 +0200

    feat [LevelSystemSignals.lua]: made some signals relevant for the system

---
+++ src/shared/Signals/LevelSystemSignals.lua
@@ -0,0 +1,14 @@
+--// Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--// Modules
+local Signal = require(ReplicatedStorage.Shared.Libs.Signal.Signal)
+
+
+local Signals = {
+    XPChanged = Signal.new(),
+    LevelUp = Signal.new(), 
+    -- add more as needed
+}
+
+return Signals
