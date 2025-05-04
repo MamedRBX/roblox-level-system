01fab2e18d7e5dbab7e12d04533934e9ece3c21c
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-04 20:57:41 +0200

    feat [LevelReward.lua]: added a optional reward microservice if a level up gives rewards

--- src/server/LevelSystem/MicroServices/LevelReward.lua
+++ src/server/LevelSystem/MicroServices/LevelReward.lua
@@ -1,5 +1,35 @@
+--// Services 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+--// Modules
+local ProfileManager = require(ServerScriptService.Server.PlayerData.ProfileManager)
+local Signals = require(ReplicatedStorage.Shared.Signals.LevelSystemSignals)
+
+
 local LevelReward = {}
 
+--// Functions
+function LevelReward.OnLevelUp(player: Player)
+    local profiles = ProfileManager.profiles[player] 
+    if not profiles then return warn("[LevelReward]: Missing Profile") end
+
+    --Here you can put in rewards that the player shoudl recevice whenever he makes a level up 
+
+    if profiles.Data.Level % 5 == 0 then
+        --maybe a reward for every 5 Levels the player makes 
+    end 
+
+
+end
+
+--// Receving Signals
+Signals.LevelUp:Connect(function(player:Player)
+    LevelReward.OnLevelUp(player)
+end)
+
+
+--// Init the Module
 function LevelReward.Init()
     
 end
