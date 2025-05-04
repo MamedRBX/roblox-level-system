0f7b28e37bc7a4e48effe6413aff7193fcfecb2a
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-04 20:57:00 +0200

    feat [LevelProgression.lua]: core logic and recevies the gainxp signal and sends the levelup signal

--- src/server/LevelSystem/MicroServices/LevelProgression.lua
+++ src/server/LevelSystem/MicroServices/LevelProgression.lua
@@ -1,5 +1,54 @@
+--// Services 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+--// Modules
+local Signals = require(ReplicatedStorage.Shared.Signals.LevelSystemSignals)
+local LevelSystemConfig = require(ReplicatedStorage.Shared.Config.LevelSystemConfig)
+local ProfileManager = require(ServerScriptService.Server.PlayerData.ProfileManager)
+
 local LevelProgression = {}
 
+--// Functions
+function LevelProgression.AdjustXp(player , amount) --adding Xp to the player 
+    local profile = ProfileManager.profiles[player]
+    while not profile do
+        profile = ProfileManager.profiles[player]
+        task.wait(0.8)
+        warn("[LevelProgression]: trying to load Profile")
+    end
+    
+
+    local currentXp = profile.Data.Xp
+    local currentLevel = profile.Data.Level
+
+    local totalXp = currentXp + amount
+
+    
+    --Leveling loop 
+    while totalXp >= LevelSystemConfig.GetXpForLevel(player , currentLevel) do
+        totalXp -= LevelSystemConfig.GetXpForLevel(player , currentLevel)
+        profile.Data.Level = currentLevel + 1
+        --sending signal that the player has leveled up
+        Signals.LevelUp:Fire(player) --> 1 stands for 1 levelup , possibly could add more 
+        
+    end
+    
+    -- Update xp
+    profile.Data.Xp = totalXp
+    player.leaderstats.Xp.Value = totalXp
+
+    --Update Level
+    player.leaderstats.Level.Value = profile.Data.Level
+end
+
+--// receving Signals
+Signals.XPChanged:Connect(function(player, amount)
+    LevelProgression.AdjustXp(player , amount)
+end)
+
+
+--// Init the Module
 function  LevelProgression.Init()
     
 end
