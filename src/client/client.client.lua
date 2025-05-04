8fc806d5bfba81edeb94b8693f88b4340bcfd59a
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-04 20:55:28 +0200

    feat [client.client.lua]: added a test file for sending a gain xp remote

--- src/client/client.client.lua
+++ src/client/client.client.lua
@@ -1,1 +1,17 @@
-print("hello world")
+--// Services 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--// RemotesFolder 
+local Remote = ReplicatedStorage.Shared.Remotes:WaitForChild("LevelRemotesFolder")
+
+local XpChange = Remote:WaitForChild("XpChange") :: RemoteEvent
+
+local XpAmount = 10
+task.wait(2 )
+while true do
+    task.wait(0.8)
+    XpChange:FireServer(XpAmount) 
+end
+
+
+
