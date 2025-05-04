dbe4046e62bba5ba8f1ea971435b189abc19149e
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-04 20:59:05 +0200

    feat [MasterySP.lua]: Levling up now increases a Mastery Skill Point

--- src/server/LevelSystem/MicroServices/MasterySP.lua
+++ src/server/LevelSystem/MicroServices/MasterySP.lua
@@ -1,5 +1,28 @@
+--// Services 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+--// Modules
+local ProfileManager = require(ServerScriptService.Server.PlayerData.ProfileManager)
+local Singals = require(ReplicatedStorage.Shared.Signals.LevelSystemSignals)
+
 local MasterySP = {}
 
+--// Functions 
+function MasterySP.OnLevelUp(player)
+    local profile = ProfileManager.profiles[player]
+    if not profile then return warn("[MasterySP]: Missing Profile") end
+
+    profile.Data.SkillPoints += 1 
+    
+end
+
+--// Receving Signals
+Singals.LevelUp:Connect(function(player:Player)
+    MasterySP.OnLevelUp(player)
+end)
+
+--// Init Module
 function  MasterySP.Init()
     
 end
