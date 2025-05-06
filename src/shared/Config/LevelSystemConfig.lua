88a8cda12a15861a8b836efcd22eecf0e4aabb59
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-06 23:36:39 +0200

    cleanup [LevelSystemConfig]: cleanedup the script , some ref and services werrent needed

--- src/shared/Config/LevelSystemConfig.lua
+++ src/shared/Config/LevelSystemConfig.lua
@@ -1,7 +1,3 @@
-local ServerScriptService = game:GetService("ServerScriptService")
-
-local ProfileManager = require(ServerScriptService.Server.PlayerData.ProfileManager)
-
 
 local LevelSystemConfig = {}
 
@@ -9,9 +5,7 @@ local Base_Xp_Cost = 100
 local XpCost_Increase_Per_Level = 25
 
 --// Functions
-function LevelSystemConfig.GetXpForLevel(player, Level:number) --Calculates how much xp the player needs for the next Level
-    local profile = ProfileManager.profiles[player]
-    if not profile then return warn("[LevelSystemConfig]: Missing Profile") end
+function LevelSystemConfig.GetXpForLevel(Level:number) --Calculates how much xp the player needs for the next Level
 
     return Base_Xp_Cost + (Level - 1) * XpCost_Increase_Per_Level
 end
