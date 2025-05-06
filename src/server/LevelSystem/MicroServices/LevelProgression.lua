ca2716ae72dc0b3e441e2a2e9c49a9109dd4fe7d
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-06 23:40:05 +0200

    cleanup [LevelProgression.server.lua]: added some typechecking and also cleaned up some fuctions and refs

--- src/server/LevelSystem/MicroServices/LevelProgression.lua
+++ src/server/LevelSystem/MicroServices/LevelProgression.lua
@@ -10,7 +10,7 @@ local ProfileManager = require(ServerScriptService.Server.PlayerData.ProfileMana
 local LevelProgression = {}
 
 --// Functions
-function LevelProgression.AdjustXp(player , amount) --adding Xp to the player 
+function LevelProgression.AdjustXp(player:Player , amount:number) --adding Xp to the player 
     local profile = ProfileManager.profiles[player]
     while not profile do
         profile = ProfileManager.profiles[player]
@@ -22,16 +22,17 @@ function LevelProgression.AdjustXp(player , amount) --adding Xp to the player
     local currentXp = profile.Data.Xp
     local currentLevel = profile.Data.Level
 
+    
+
     local totalXp = currentXp + amount
 
     
     --Leveling loop 
-    while totalXp >= LevelSystemConfig.GetXpForLevel(player , currentLevel) do
-        totalXp -= LevelSystemConfig.GetXpForLevel(player , currentLevel)
+    while totalXp >= LevelSystemConfig.GetXpForLevel( currentLevel) do
+        totalXp -= LevelSystemConfig.GetXpForLevel(currentLevel)
         profile.Data.Level = currentLevel + 1
         --sending signal that the player has leveled up
         Signals.LevelUp:Fire(player) --> 1 stands for 1 levelup , possibly could add more 
-        
     end
     
     -- Update xp
@@ -40,10 +41,19 @@ function LevelProgression.AdjustXp(player , amount) --adding Xp to the player
 
     --Update Level
     player.leaderstats.Level.Value = profile.Data.Level
+
+    --Updating Ui 
+    local UdapteUiRemote  = ReplicatedStorage.Shared.Remotes:WaitForChild("LevelRemotesFolder"):WaitForChild("UpdateUi") :: RemoteEvent 
+    UdapteUiRemote:FireClient(player, "Update", {
+     Xp = profile.Data.Xp ,
+     Level = profile.Data.Level ,
+    }) --sending Xp and Level value to the StateManager to update
+ 
 end
 
 --// receving Signals
-Signals.XPChanged:Connect(function(player, amount)
+Signals.XPChanged:Connect(function(player:Player, amount:number)
+
     LevelProgression.AdjustXp(player , amount)
 end)
 
