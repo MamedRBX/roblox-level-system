893cac056a9e4cffd0f457774cba883dd6c46cdf
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-06 23:35:14 +0200

    refactor [StateManager.lua]: implemented fusion and licked the ui update remoteevent

--- src/shared/Client/StateManager.lua
+++ src/shared/Client/StateManager.lua
@@ -1,28 +1,46 @@
+
 --// Services 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 --// Modules 
 local Template = require(ReplicatedStorage.Shared.PlayerData.template)
+local Fusion = require(ReplicatedStorage._Packages.Fusion)
+
+--//Remotes 
+local UpdateUiRemote  = ReplicatedStorage.Shared.Remotes:WaitForChild("LevelRemotesFolder"):WaitForChild("UpdateUi") :: RemoteEvent
 
 local isDataLoaded = false
 
 local PlayerData: Template.PlayerData
 
+--// Fusion Variables 
+local Value = Fusion.Value
+
+--//Functions
 local function LoadData()
 	if isDataLoaded then return end 
-
+	local GetAllData = ReplicatedStorage.Shared.Remotes:WaitForChild("GetAllData") :: RemoteFunction
 	while not PlayerData do
-		PlayerData = ReplicatedStorage.Events.GetAllData:InvokeServer()
+		PlayerData = GetAllData:InvokeServer()
 		task.wait(1)
 
 	end
 	isDataLoaded = true	
 end
 
+
+--// load all the data
 LoadData()
+
 
 local StateManager = {}
 
+--// Loading Fusion Values
+function StateManager.InitReactiveValues()
+	StateManager.Level = Value(PlayerData.Level)
+	StateManager.Xp = Value(PlayerData.Xp)
+	--put all the changable values of the player right here
+end
 
 function StateManager.GetData(): Template.PlayerData
 	while not isDataLoaded do
@@ -31,5 +49,19 @@ function StateManager.GetData(): Template.PlayerData
 	end
 	return PlayerData
 end
+
+
+--// Recevie Remotes
+UpdateUiRemote.OnClientEvent:Connect(function(key:string , payload: {})
+	if not key then return warn("[StateManager]:Missing key") end
+	if not payload then return warn("[StateManager]: Missing payload") end
+	if key == "Update" then
+        for stat, value in pairs(payload) do
+            if StateManager[stat] then
+                StateManager[stat]:set(value)
+            end
+        end
+    end
+end)
 
 return StateManager
