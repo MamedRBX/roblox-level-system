1b9f1b8e4e2dd693aa75cd8aa5e9018a10aaf318
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-08 17:07:00 +0200

    feat [LevelSystemConfig.lua]: added a function to make a cap for the levels

--- src/shared/Config/LevelSystemConfig.lua
+++ src/shared/Config/LevelSystemConfig.lua
@@ -3,6 +3,7 @@ local LevelSystemConfig = {}
 
 local Base_Xp_Cost = 100
 local XpCost_Increase_Per_Level = 25
+local SkillCap = 100
 
 --// Functions
 function LevelSystemConfig.GetXpForLevel(Level:number) --Calculates how much xp the player needs for the next Level
@@ -11,5 +12,16 @@ function LevelSystemConfig.GetXpForLevel(Level:number) --Calculates how much xp 
 end
 
 
+function LevelSystemConfig.SkillCap(amountToAdd:number,SkillAmount:number) --Just gives the cap of the SKill , could be extandend
+    local total = SkillAmount + amountToAdd
+
+    if SkillAmount >= SkillCap then
+        return 0
+    elseif total > SkillCap then
+        return SkillCap - SkillAmount
+    else
+        return amountToAdd
+    end
+end 
 
 return  LevelSystemConfig
