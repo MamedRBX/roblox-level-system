7c3e7e2760f269a6cbd2fa3d8135ea7489e6bf1f
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-08 17:01:08 +0200

    feat [LevelSystem.client.lua]: added refs , variables , also the textlabel for the level , some recevers , and button actions

--- src/client/LevelSystem.client.lua
+++ src/client/LevelSystem.client.lua
@@ -2,44 +2,53 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
+
 --// Modules
 local StateManager = require(ReplicatedStorage.Shared.Client.StateManager)
 local Fusion = require(ReplicatedStorage._Packages.Fusion)
 local LevelSystemConfig = require(ReplicatedStorage.Shared.Config.LevelSystemConfig)
-
+local Tweens = require(script.Parent.Tweens)
+local Signals = require(ReplicatedStorage.Shared.Signals.LevelSystemSignals)
 
 --// Folders 
-local LevelRemotes = ReplicatedStorage.Shared.Remotes:WaitForChild("LevelRemotesFolder")
+
+
 
 --//init StateManager with Fusion variables
 StateManager.InitReactiveValues()
 
 
---// Refs 
+--// [Refs]
 
 --// Main Refs
 local player  = Players.LocalPlayer
 local PlayerGui = player:WaitForChild("PlayerGui")
-
 local LevelingSystem = PlayerGui:WaitForChild("LevelingSystem")
-
 local MainFrame = LevelingSystem.MainFrame
+local MasterySP = LevelingSystem.MasterySP
 
---// LevelLable
-local XPCounter = MainFrame.Level.XpCounter
-local LevelCounter = MainFrame.Level.LevelCounter
+--//MasterySP
+local Holder = MasterySP.Holder
+
 
 
 --//XP Display 
 local XpBarBackground = MainFrame.XPDisplay.XPBarBackground
-local XpBar = MainFrame.XPDisplay.XPBarBackground.XPBar
+
 
 --// Fusion variables
 local New = Fusion.New
 local Computed = Fusion.Computed
+local Value = Fusion.Value
+
+--// Buttons
+local infoButton = MasterySP.InfoButton
+local closeButton = MasterySP.closeButton
+local OpenButton = LevelingSystem.MasteryButton
 
 
 
+--// TextLabel for the Xp Counter
 local XpCounter = New "TextLabel" {  --Xp Counter Label that displayes the current Xp and the Xp requirement for the next Level 
 
     Name = "XpCounter",
@@ -61,8 +70,67 @@ local XpCounter = New "TextLabel" {  --Xp Counter Label that displayes the curre
 
 
 }
-local UiStroke = Instance.new("UIStroke", XpCounter)
+
+local UiStroke = Instance.new("UIStroke", XpCounter) 
 UiStroke.Color = Color3.fromRGB(67, 73, 104)
 UiStroke.Thickness = 1.5
 
 
+--// TextLabel for the Level Counter
+local LevelCounter = New "TextLabel"  {
+    Name = "LevelConuter",
+    Parent = LevelingSystem.MainFrame.Level,
+    BackgroundTransparency = 1,
+    TextScaled = true,
+    Font = Enum.Font.GothamBold,
+    TextColor3 = Color3.fromRGB(227, 227, 0),
+    Position = UDim2.fromScale(0.299, 0.347),
+    Size = UDim2.fromScale(0.395,0.299),
+
+    Text = Computed(function()
+        return StateManager.Level:get()       
+    end)
+}
+
+local UiStrokeLevelCounter = Instance.new("UIStroke",LevelCounter)
+UiStrokeLevelCounter.Color = Color3.fromRGB(255, 255, 127)
+UiStrokeLevelCounter.Thickness = 0.6
+
+
+--// Frame for the XPBar 
+local XpBarFill = New "Frame" {
+    Name = "XpBar",
+    Parent = XpBarBackground,
+    BackgroundColor3 = Color3.fromRGB(0,255,0),
+    BorderSizePixel = 0,
+    Position = UDim2.fromScale(0, 0),
+    Size = Fusion.Spring(Computed(function()
+        local currentXp = StateManager.Xp:get()
+        local maxXp = LevelSystemConfig.GetXpForLevel(StateManager.Level:get())
+        local fillRatio = math.clamp(currentXp / maxXp, 0, 1)
+        return UDim2.fromScale(fillRatio, 1)
+    end), 15) 
+}
+
+
+--//Button actions
+OpenButton.MouseButton1Up:Connect(function()
+    MasterySP.Visible = not MasterySP.Visible
+end)
+
+closeButton.MouseButton1Up:Connect(function()
+    MasterySP.Visible = not MasterySP.Visible
+end)
+
+infoButton.MouseButton1Up:Connect(function()
+    --show infos 
+end)
+
+--// Receving Signals
+Signals.XpPopUp:Connect(function(old:number , new:number)
+    Tweens.showXpGainPopup(new - old) --diplay the xp after getting xp
+end)
+
+Signals.LevelPopUp:Connect(function()
+    Tweens.showLevelGainPopup() --displaying the Level after levelup
+end)
