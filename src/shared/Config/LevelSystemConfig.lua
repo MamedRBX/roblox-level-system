a3a5441595f182e052647c1079b59208fc962cd6
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-10 21:45:34 +0200

    feat [LevelSystemConfig]: added a bunch of functions, also more comments , and more base variables

--- src/shared/Config/LevelSystemConfig.lua
+++ src/shared/Config/LevelSystemConfig.lua
@@ -1,53 +1,78 @@
 
-local LevelSystemConfig = {}
+local LevelSystemConfig = {} --Start of the Module
 
+--// Base values
 local Base_Xp_Cost = 100
 local XpCost_Increase_Per_Level = 25
 local SkillCap = 100
+local LevelCap = 100
+local milestones = {25, 50, 75, 100}
+
 
 --// Functions
 function LevelSystemConfig.GetXpForLevel(Level:number) --Calculates how much xp the player needs for the next Level
-
-    return Base_Xp_Cost + (Level - 1) * XpCost_Increase_Per_Level
+	if Level == LevelCap then
+		return Base_Xp_Cost + (Level - 2) * XpCost_Increase_Per_Level
+	else
+    	return Base_Xp_Cost + (Level - 1) * XpCost_Increase_Per_Level
+	end
 end
 
 
-function LevelSystemConfig.SkillCap(amountToAdd:number,SkillAmount:number) --Just gives the cap of the SKill , could be extandend
-    local total = SkillAmount + amountToAdd
-
-    if SkillAmount >= SkillCap then
-        return 0
-    elseif total > SkillCap then
-        return SkillCap - SkillAmount
-    else
-        return amountToAdd
-    end
+function LevelSystemConfig.SkillCap(SkillAmount:number) --Just gives the cap of the Skill , could be extandend
+   if SkillAmount < SkillCap then
+		return true --player can skill
+   else
+		return false --player is at Skillcap
+   end 
 end 
 
 
-function LevelSystemConfig.MileStoneCalc(LevelAmount: number): number --return the amount to the next Milestole
-	local milestones = {25, 50, 75, 100}
-	for _, milestone in ipairs(milestones) do
-		if LevelAmount < milestone then
-			return milestone
+function LevelSystemConfig.GetMileStoneStart(LevelAmount: number) : number --gies the starting point for the Skill Progress bar
+	for index, value in milestones do
+		if LevelAmount < value then
+			if index > 1 then
+				return milestones[index - 1] --if the current milestone goal is 50 , then this will give 25 
+			else
+				
+				return 0 -- if its the first milestone , then we just return normally 
+			end
+		end
+		if LevelAmount == SkillCap then
+			return value
 		end
 	end
 	warn("[LevelSystemConfig]: Level exceeds all milestones")
-    return #milestones
+    return 
 end
 
-function LevelSystemConfig.MileStoneNumber(LevelAmount: number): number
-    local milestones = {25, 50, 75, 100}
-	for index, milestone in ipairs(milestones) do
+function LevelSystemConfig.GetMileStoneEnd(LevelAmount: number): number --gives the end point for the Skill Progress bar
+	for _, milestone in milestones do
+		if LevelAmount < milestone then
+			return milestone
+		elseif LevelAmount == SkillCap then
+		
+			return SkillCap
+
+		end		
+			
+	end
+
+end
+
+function LevelSystemConfig.MileStoneNumber(LevelAmount: number): number --returns the MileStone tier of that Skill 
+	for index, milestone in milestones do
 		if LevelAmount < milestone then
 			return index
 		end
 	end
-	warn("[LevelSystemConfig]: Level exceeds all milestones")
     return #milestones
 end
 
 
+function LevelSystemConfig.LevelCap(Level: number) : boolean  --returns if the player can get another Level
+	if Level >= LevelCap then return false else return true end
+end
 
 
 
