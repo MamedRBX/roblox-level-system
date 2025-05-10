506cc2942fc9e2f72ba3a6a5d30cacdb25b97f0d
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-10 21:36:13 +0200

    refactor [testGiveXP]: added some refs and structure and also a validation check

--- src/client/testGiveXp.client.lua
+++ src/client/testGiveXp.client.lua
@@ -1,14 +1,28 @@
+--// Services 
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+--//Refs
 local player = Players.LocalPlayer
+
+--//Modules
+local StateManager = require(ReplicatedStorage.Shared.Client.StateManager)
+local LevelSystemConfig = require(ReplicatedStorage.Shared.Config.LevelSystemConfig)
+
+
+--// Remotes
 local GiveXP = ReplicatedStorage.Shared.Remotes:WaitForChild("LevelRemotesFolder"): WaitForChild("XpChange") :: RemoteEvent
 
 
-task.wait(3)
+
+
+task.wait(5)
 
 while true do
-    GiveXP:FireServer(50)
-    task.wait(0.2)
+    if LevelSystemConfig.LevelCap(StateManager.Level:get()) then
+        GiveXP:FireServer(300)
+    end
+    
+    task.wait(0.1)
 end
 
