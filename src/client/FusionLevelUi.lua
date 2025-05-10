8214efe22992f4adf6429b3b45bc58158d1d7952
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-10 21:33:58 +0200

    feat [Fusion]: Now adds a skkillbutton over the skill if it can be skilled

--- src/client/FusionLevelUi.lua
+++ src/client/FusionLevelUi.lua
@@ -8,10 +8,17 @@ local LevelSystemConfig = require(ReplicatedStorage.Shared.Config.LevelSystemCon
 local Fusion = require(ReplicatedStorage._Packages.Fusion)
 local StateManager = require(ReplicatedStorage.Shared.Client.StateManager)
 
+--// Folder
+local LevelRemotesFolder = ReplicatedStorage.Shared.Remotes:WaitForChild("LevelRemotesFolder") :: Folder
+
+--// Remotes
+local SpendMasterySP = LevelRemotesFolder:WaitForChild("SpendMasterySP") :: RemoteEvent
+
 --// Fusion variables
 local Spring = Fusion.Spring
 local New = Fusion.New
 local Computed = Fusion.Computed
+local OnEvent = Fusion.OnEvent
 
 local FusionLevelUi = {} --Start of the Module
 
@@ -21,14 +28,13 @@ function FusionLevelUi.CreateSkillCounterUi(skills: {}) --goes through a list of
 		FusionLevelUi.SkillCounterUi(Skill)
 		FusionLevelUi.MileStoneCounterUi(Skill)
 		FusionLevelUi.SkillProgressBarUi(Skill)
+		FusionLevelUi.SkillButton(Skill)
 	end
 end
 
 
 function FusionLevelUi.SkillCounterUi(parent: Frame) --creates a TextLabel for displaying the amount of Skillpoints on a Skill
 	local skillName = parent.Name
-	local mastery = StateManager.Masteries[skillName]
-	local smoothVal = Spring(mastery, 30, 1)
 
 	return New "TextLabel" {
 		Name = skillName .. "LevelCounter",
@@ -42,31 +48,36 @@ function FusionLevelUi.SkillCounterUi(parent: Frame) --creates a TextLabel for d
 		TextColor3 = Color3.fromRGB(255, 255, 255),
 
 		Text = Computed(function()
-			local baseValue = smoothVal:get()
-			return math.floor(baseValue) .. " / " .. LevelSystemConfig.MileStoneCalc(math.floor(baseValue))
+			local mastery = StateManager.Masteries[skillName]
+			if not mastery then return "??" end
+
+			return math.floor(mastery:get()) .. " / " .. LevelSystemConfig.GetMileStoneEnd(math.floor(mastery:get()))
 		end)
 	}
 end
 
 
-function FusionLevelUi.SkillProgressBarUi(parent: Frame)
+function FusionLevelUi.SkillProgressBarUi(parent: Frame) --ProgressBar for Skills
+	
 	return New "Frame" {
 		Name = parent.Name .. "ProgressBar",
-		Parent = parent,
+		Parent = parent.BarBackground,
 		AnchorPoint = Vector2.new(0,0),
 		Position = UDim2.fromScale(0,0),
 		BackgroundTransparency = 0,
 		BackgroundColor3 = Color3.fromRGB(0,255,0),
 		Size = Spring(Computed(function()
-        local currentLevel = StateManager.Masteries[parent.Name]:get()
-        local maxLevel = LevelSystemConfig.MileStoneCalc(StateManager.Masteries[parent.Name]:get())
-        local fillRatio = math.clamp(currentLevel / maxLevel, 0, 1)
-        return UDim2.fromScale(fillRatio, 1)
-    end), 15) 
+			local lvl:number = StateManager.Masteries[parent.Name]:get()
+			local floor = LevelSystemConfig.GetMileStoneStart(lvl)
+			local ceil = LevelSystemConfig.GetMileStoneEnd(lvl)   
+			local progress = (lvl - floor) / (ceil - floor)
+
+			return UDim2.fromScale(math.clamp(progress, 0, 1), 1)
+		end), 15) 
 	}
 end
 
-function FusionLevelUi.MileStoneCounterUi(parent:Frame)
+function FusionLevelUi.MileStoneCounterUi(parent:Frame) --Milestone for Skills
 	return New "TextLabel" {
 		Name = parent.Name .. "MileStone",
 		Parent = parent,
@@ -81,6 +92,42 @@ function FusionLevelUi.MileStoneCounterUi(parent:Frame)
 		
 			return "MILESTONE "..LevelSystemConfig.MileStoneNumber(StateManager.Masteries[parent.Name]:get())
 		end)
+	}
+end
+
+function FusionLevelUi.SkillButton(parent: Frame)--SkillButtons for Skills
+	return New "ImageButton" {
+		Name = parent.Name.."SkillButton",
+		Parent = parent,
+		Image = "rbxassetid://118936424993971",
+		AnchorPoint = Vector2.new(0,0),
+		Position = UDim2.fromScale(0.087, 0.223),
+		Size = UDim2.fromScale(0.083,0.512),
+		BackgroundTransparency = 1,
+
+		Visible  = Computed(function()
+			local display = false
+			if StateManager.SkillPoints:get() <= 0 then
+				display = false
+			else
+				if StateManager.Masteries[parent.Name]:get() < 100 then
+					display = true
+				else
+					display = false
+				end
+			end
+			return display
+			
+		end),
+		[OnEvent "Activated"] = function()
+		if StateManager.SkillPoints:get() >= 1 then --quick validation
+
+			SpendMasterySP:FireServer(1, parent.Name) --point spend
+		else
+			--possible popup for "Not enough skillpoints"
+			print("not enouth SkillPoints")
+		end
+	end
 	}
 end
 
