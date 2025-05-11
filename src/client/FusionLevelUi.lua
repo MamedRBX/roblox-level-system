556635ef99b85ca96dbd11a680ee5dba291a83bf
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-11 03:17:32 +0200

    feat [FusionLevelUi]: created new buttons and also extended the SkillPointSpend logic , also added sounds

--- src/client/FusionLevelUi.lua
+++ src/client/FusionLevelUi.lua
@@ -1,6 +1,6 @@
 --// Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
+local SoundService = game:GetService("SoundService")
 
 --// Modules
 local FormatNumber = require(ReplicatedStorage.Shared.Libs.FormatNumberShort)
@@ -10,6 +10,11 @@ local StateManager = require(ReplicatedStorage.Shared.Client.StateManager)
 
 --// Folder
 local LevelRemotesFolder = ReplicatedStorage.Shared.Remotes:WaitForChild("LevelRemotesFolder") :: Folder
+local SoundFolder = SoundService:WaitForChild("Sounds"):WaitForChild("LevelSystemSounds") :: Folder
+
+--//Sounds
+local SkillPointSpend = SoundFolder.SkillPointSpend :: Sound
+local ClickSound = SoundFolder.ClickSounds :: Sound
 
 --// Remotes
 local SpendMasterySP = LevelRemotesFolder:WaitForChild("SpendMasterySP") :: RemoteEvent
@@ -19,6 +24,7 @@ local Spring = Fusion.Spring
 local New = Fusion.New
 local Computed = Fusion.Computed
 local OnEvent = Fusion.OnEvent
+local Children = Fusion.Children
 
 local FusionLevelUi = {} --Start of the Module
 
@@ -64,6 +70,12 @@ function FusionLevelUi.SkillProgressBarUi(parent: Frame) --ProgressBar for Skill
 		Parent = parent.BarBackground,
 		AnchorPoint = Vector2.new(0,0),
 		Position = UDim2.fromScale(0,0),
+		[Children]= {
+			New "UICorner" {
+				CornerRadius = UDim.new(0, 8)
+				
+			}
+		},
 		BackgroundTransparency = 0,
 		BackgroundColor3 = Color3.fromRGB(0,255,0),
 		Size = Spring(Computed(function()
@@ -75,6 +87,8 @@ function FusionLevelUi.SkillProgressBarUi(parent: Frame) --ProgressBar for Skill
 			return UDim2.fromScale(math.clamp(progress, 0, 1), 1)
 		end), 15) 
 	}
+	
+
 end
 
 function FusionLevelUi.MileStoneCounterUi(parent:Frame) --Milestone for Skills
@@ -120,9 +134,16 @@ function FusionLevelUi.SkillButton(parent: Frame)--SkillButtons for Skills
 			
 		end),
 		[OnEvent "Activated"] = function()
+		ClickSound:Play()
 		if StateManager.SkillPoints:get() >= 1 then --quick validation
-
-			SpendMasterySP:FireServer(1, parent.Name) --point spend
+			if StateManager.SelectedSpendAmount:get() % 1 == 0 and not StateManager.SelectedSpendAmount:get() == 0 then
+				SkillPointSpend:Play()
+				SpendMasterySP:FireServer(StateManager.SelectedSpendAmount:get(), parent.Name) --point spend
+			else
+				SkillPointSpend:Play()
+				SpendMasterySP:FireServer(1, parent.Name)
+			end
+			
 		else
 			--possible popup for "Not enough skillpoints"
 			print("not enouth SkillPoints")
@@ -131,4 +152,48 @@ function FusionLevelUi.SkillButton(parent: Frame)--SkillButtons for Skills
 	}
 end
 
+
+function FusionLevelUi.CreatingSkillAmountButtons(parent:Frame, amount: number)
+
+	return New "TextButton" {
+
+		Name = parent.Name,
+		Parent = parent,
+		Text = "x"..amount,
+		AnchorPoint = Vector2.new(0.5,0.5),
+		Position = UDim2.fromScale(0.5, 0.5),
+		Size = UDim2.fromScale(1,1),
+		BackgroundTransparency = 0,
+		TextColor3 = Color3.fromRGB(74, 55, 74),
+		Font = Enum.Font.GothamBold,
+		TextScaled = true,
+		BackgroundColor3 = Color3.fromRGB(150, 112, 150),
+		Visible = true,
+		[Children] = {
+			New "UIStroke" {
+				Thickness = 2, 
+				ApplyStrokeMode = Enum.ApplyStrokeMode.Border,
+				Transparency = 0, 
+				Color = Computed(function()
+					if StateManager.SelectedSpendAmount:get() == amount then
+						return Color3.fromRGB(227, 227, 0)
+					else
+						return Color3.fromRGB(176, 132, 176)
+					end 
+				end)
+				
+			},
+
+			New "UICorner" {
+				CornerRadius = UDim.new(0, 3),
+			}
+		},
+		
+		[OnEvent "Activated"] = function()
+			ClickSound:Play()
+			StateManager.SelectedSpendAmount:set(amount)
+			StateManager.CustomSpendText:set(tostring(amount))
+		end
+	}
+end
 return FusionLevelUi
