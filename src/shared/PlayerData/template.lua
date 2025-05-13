25985cbb261bacfd9f37d19e4dd3179efec7158c
MamedRBX <mamedmamedoglu12345@gmail.com>
2025-05-13 22:38:19 +0200

    feat [template]; expanded the template , so that it saves all the Skills the player owns

--- src/shared/PlayerData/template.lua
+++ src/shared/PlayerData/template.lua
@@ -1,24 +1,32 @@
-
 export type SkillLevels = {
-    Strength: number,
-    Wisdom: number,
-    Stamina: number,
-    Luck: number,
+	Strength: number,
+	Wisdom: number,
+	Stamina: number,
+	Luck: number,
 }
 
-local SkillLevels:SkillLevels = {
+export type SkillTreeEntry = {
+	Level: number,
+}
+
+export type SkillTree = {
+	[string]: SkillTreeEntry
+}
+
+local SkillLevels: SkillLevels = {
 	Strength = 0,
 	Wisdom = 0,
 	Stamina = 0,
 	Luck = 0,
-} 
+}
 
 local Template = {
 	SkillTreePoints = 0, 
-	SkillPoints = 0, 
+	SkillPoints = 0,    
 	Level = 1,
-	Xp = 0, 
-	Skills = SkillLevels
+	Xp = 0,
+	Skills = SkillLevels,
+	SkillTreeSkills = {} :: SkillTree,
 }
 
 export type PlayerData = typeof(Template)
